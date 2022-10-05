884d7b48469f83814098215e57e997d348f4a3a9
avishka05 <it20642914@my.sliit.lk>
2022-10-05 13:14:35 +0530

    creating Springboot project

---
+++ empmanagement/src/main/java/com/empmanagement/empmanagement/EmpmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.empmanagement.empmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpmanagementApplication.class, args);
+	}
+
+}
