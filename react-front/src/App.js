b64ae91ee42c7220ecee9b328e2310d5cabb2dd0
avishka05 <it20642914@my.sliit.lk>
2022-10-07 10:05:18 +0530

    add employee from  implementation

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,27 +1,27 @@
 
 import './App.css';
 import ListEmployeeComponent from './Components/ListEmployeeComponents';
-import { BrowserRouter as Router,Routes, Route,  } from 'react-router-dom';
+import AddEmployee from './Components/AddEmployee/AddEmployee';
+
 import Header from './layout/header/Header';
+import { BrowserRouter as Router,Routes, Route,  } from 'react-router-dom';
+import EditeEmployee from './Components/EditeEmployee/EditeEmployee';
 function App() {
   return (
     <div className="App">    
 
 
     
-      <Router>
-      <Header/>
-      <ListEmployeeComponent />
-      <header className="container">
+  <Router>
+    <Header/>
 
-
-      <Routes> 
-      
-      
+       <Routes>    
+            < Route exact path='/' element={<ListEmployeeComponent />}> </Route>
+            < Route exact path='/add' element={<AddEmployee />}> </Route>
+            < Route exact path='/edite' element={<EditeEmployee />}> </Route>
+        </Routes>
         
-         </Routes>
-         </header>
-      </Router>
+  </Router>
  
      
     </div>
